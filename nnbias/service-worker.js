b71872a62d3c1f93d6fb8f50914880c76b20b43b
Guillermo Valle <guillefix@gmail.com>
2019-05-06 05:27:49 +0100

    made nnbias more desktop friendly

--- nnbias/service-worker.js
+++ nnbias/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f5ca3060ad728417a341ba536806a7e3.js"
+  "./precache-manifest.300426d0224381446b2d9edf03e3d411.js"
 );
 
 workbox.clientsClaim();
