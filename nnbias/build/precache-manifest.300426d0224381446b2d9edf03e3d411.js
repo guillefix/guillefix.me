3ea7d4a3b0df49fbb34c20e316d598101e94b3c9
Guillermo Valle <guillefix@gmail.com>
2019-05-06 05:19:32 +0100

    made nnbias more desktop friendly

---
+++ nnbias/build/precache-manifest.300426d0224381446b2d9edf03e3d411.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "30576c50ded412eff5a5",
+    "url": "./static/js/main.c745d6fc.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "./static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "30576c50ded412eff5a5",
+    "url": "./static/css/main.0e075d2f.chunk.css"
+  },
+  {
+    "revision": "ebda124ed02b75d3665ad9a25ce50343",
+    "url": "./index.html"
+  }
+];
